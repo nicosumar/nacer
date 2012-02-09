ad00b34d756ea118f4348ee8d4dd0e4f3f72d395
Santiago Bosio <santiago.bosio@gmail.com>
2012-02-09 17:52:26 -0300

    Modificación en diccionarios FTS y modelo de búsquedas para implementar búsquedas aproximadas en cadenas utilizando una función de distancia de Levenshtein ponderada

--- app/models/busqueda.rb
+++ app/models/busqueda.rb
@@ -1,11 +1,24 @@
 class Busqueda < ActiveRecord::Base
 
   def self.busqueda_fts(terminos)
-    terminos_sql = sanitize_sql_for_conditions(['?', terminos])
+    tsquery = self.texto_a_consulta_fts(terminos)
     Busqueda.find_by_sql("
       SELECT * FROM busquedas
-        WHERE plainto_tsquery('public.es_ar', #{terminos_sql}) @@ vector_fts
-        ORDER BY ts_rank(vector_fts, plainto_tsquery('public.es_ar', #{terminos_sql})) DESC;")
+        WHERE '#{tsquery}'::tsquery @@ vector_fts
+        ORDER BY ts_rank(vector_fts, '#{tsquery}'::tsquery) DESC;")
+  end
+
+private
+  def self.texto_a_consulta_fts(terminos)
+    terminos_sql = sanitize_sql_for_conditions(['?', terminos])
+    lexemas = connection.execute("SELECT lexemes FROM ts_debug('public.es_ar', #{terminos_sql});")
+    tsquery = []
+    lexemas.column_values(0).each do |l|
+      if not l.blank?
+        tsquery << l.gsub(/\{/, "(").gsub(/,/, " | ").gsub(/\}/, ")")
+      end
+    end
+    return tsquery.join(" & ")
   end
 
 end
