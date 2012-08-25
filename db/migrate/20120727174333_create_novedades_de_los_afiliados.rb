1eb067bd4f523b0bd6e7ffd2c26347b589621217
Santiago Bosio <santiago.bosio@gmail.com>
2012-08-25 19:39:24 -0300

    Modificaciones de las estructuras de la tablas de afiliados.

--- db/migrate/20120727174333_create_novedades_de_los_afiliados.rb
+++ db/migrate/20120727174333_create_novedades_de_los_afiliados.rb
@@ -1,67 +1,98 @@
 class CreateNovedadesDeLosAfiliados < ActiveRecord::Migration
   def change
     create_table :novedades_de_los_afiliados do |t|
-      t.references :centro_de_inscripcion, :null => false
+
+      # Tipo y estado de la novedad
       t.references :tipo_de_novedad, :null => false
       t.references :estado_de_la_novedad, :null => false
-      t.string :clave_de_beneficiario, :null => false
+
+      # Identificadores
+      t.string :clave_de_beneficiario, :null => false, :unique => true
+
+      # Datos personales, nivel de alfabetización y discapacidad
       t.string :apellido
       t.string :nombre
-      t.references :tipo_de_documento
       t.references :clase_de_documento
+      t.references :tipo_de_documento
       t.string :numero_de_documento
-      t.references :sexo
+      t.string :numero_de_celular
+      t.string :e_mail
+
+      # Mantenemos la categoría de afiliado por razones de compatibilidad
       t.references :categoria_de_afiliado
+
+      # Datos de nacimiento, sexo, origen y estudios
+      t.references :sexo
       t.date :fecha_de_nacimiento
       t.integer :pais_de_nacimiento_id
       t.boolean :se_declara_indigena
       t.references :lengua_originaria
       t.references :tribu_originaria
-      t.integer :tipo_de_documento_de_la_madre_id
-      t.string :numero_de_documento_de_la_madre
-      t.string :apellido_de_la_madre
-      t.string :nombre_de_la_madre
-      t.integer :tipo_de_documento_del_padre_id
-      t.string :numero_de_documento_del_padre
-      t.string :apellido_del_padre
-      t.string :nombre_del_padre
-      t.integer :tipo_de_documento_del_tutor_id
-      t.string :numero_de_documento_del_tutor
-      t.string :apellido_del_tutor
-      t.string :nombre_del_tutor
-      t.date :fecha_de_diagnostico_del_embarazo
-      t.integer :semanas_de_embarazo
-      t.date :fecha_probable_de_parto
-      t.date :fecha_efectiva_de_parto
+      t.integer :alfabetizacion_del_beneficiario_id
+      t.integer :alfab_beneficiario_años_ultimo_nivel
+
+      # Datos de residencia, vías de comunicación y lugar habitual de atención
       t.string :domicilio_calle
       t.string :domicilio_numero
-      t.string :domicilio_manzana
       t.string :domicilio_piso
       t.string :domicilio_depto
+      t.string :domicilio_manzana
       t.string :domicilio_entre_calle_1
       t.string :domicilio_entre_calle_2
-      t.string :domicilio_barrio_o_paraje
+      t.string :telefono
+      t.string :otro_telefono
       t.integer :domicilio_provincia_id
       t.integer :domicilio_departamento_id
       t.integer :domicilio_distrito_id
+      t.string :domicilio_localidad
+      t.string :domicilio_barrio_o_paraje
       t.string :domicilio_codigo_postal
-      t.string :telefono
-      t.references :efector
-      t.date :fecha_de_la_novedad, :null => false
-      t.string :score_de_riesgo
-      t.integer :alfabetizacion_id
-      t.integer :años_en_el_ultimo_nivel
+      t.text :observaciones
+
+      # Lugar de atención habitual
+      t.string :cuie_del_lugar_de_atencion_habitual
+
+      # Datos del adulto responsable del menor (para menores de 14 años)
+      t.string :apellido_de_la_madre
+      t.string :nombre_de_la_madre
+      t.integer :tipo_de_documento_de_la_madre_id
+      t.string :numero_de_documento_de_la_madre
       t.integer :alfabetizacion_de_la_madre_id
-      t.integer :años_en_el_ultimo_nivel_de_la_madre
+      t.integer :alfab_madre_años_ultimo_nivel
+      t.string :apellido_del_padre
+      t.string :nombre_del_padre
+      t.integer :tipo_de_documento_del_padre_id
+      t.string :numero_de_documento_del_padre
       t.integer :alfabetizacion_del_padre_id
-      t.integer :años_en_el_ultimo_nivel_del_padre
+      t.integer :alfab_padre_años_ultimo_nivel
+      t.string :apellido_del_tutor
+      t.string :nombre_del_tutor
+      t.integer :tipo_de_documento_del_tutor_id
+      t.string :numero_de_documento_del_tutor
       t.integer :alfabetizacion_del_tutor_id
-      t.integer :años_en_el_ultimo_nivel_del_tutor
-      t.string :e_mail
-      t.string :numero_de_celular
-      t.date :fecha_de_ultima_menstruacion
+
+      # Datos del embarazo y parto (para embarazadas)
+      t.date :fecha_de_la_ultima_menstruacion
+      t.date :fecha_de_diagnostico_del_embarazo
+      t.integer :semanas_de_embarazo
+      t.date :fecha_probable_de_parto
+      t.date :fecha_efectiva_de_parto  # Dato mantenido por compatibilidad
+
+      # Score de riesgo cardiovascular del Programa Remediar+Redes
+      t.integer :score_de_riesgo
+
+      # Discapacidad
+      t.string :discapacidad
+
+      # Fecha y centro inscriptor
+      t.date :fecha_de_inscripcion
+      t.references :centro_de_inscripcion
+      t.string :nombre_del_agente_inscriptor
+
+      # Observaciones generales
       t.text :observaciones_generales
-      t.references :discapacidad
+
+      # Datos relacionados con la carga del registro
       t.string :created_by
       t.string :updated_by
       t.timestamps
--- db/migrate/20120824095905_recreate_afiliados.rb
+++ db/migrate/20120824095905_recreate_afiliados.rb
@@ -5,81 +5,103 @@ class RecreateAfiliados < ActiveRecord::Migration
 
     # Creamos la tabla con la nueva estructura
     create_table :afiliados, :id => false do |t|
+
+      # Identificadores
       t.integer :afiliado_id, :null => false, :unique => true
       t.string :clave_de_beneficiario, :null => false, :unique => true
+
+      # Datos personales, nivel de alfabetización y discapacidad
       t.string :apellido
       t.string :nombre
-      t.references :tipo_de_documento
       t.references :clase_de_documento
+      t.references :tipo_de_documento
       t.string :numero_de_documento
-      t.references :sexo_id
+      t.string :numero_de_celular
+      t.string :e_mail
+
+      # Mantenemos la categoría de afiliado por razones de compatibilidad
       t.references :categoria_de_afiliado
+
+      # Datos de nacimiento, sexo, origen y estudios
+      t.references :sexo
       t.date :fecha_de_nacimiento
       t.integer :pais_de_nacimiento_id
-      t.integer :provincia_de_nacimiento_id
-      t.integer :departamento_de_nacimiento_id
       t.boolean :se_declara_indigena
       t.references :lengua_originaria
       t.references :tribu_originaria
-      t.integer :tipo_de_documento_de_la_madre_id
-      t.string :numero_de_documento_de_la_madre
+      t.integer :alfabetizacion_del_beneficiario_id
+      t.integer :alfab_beneficiario_años_ultimo_nivel
+
+      # Datos de residencia, vías de comunicación y lugar habitual de atención
+      t.string :domicilio_calle
+      t.string :domicilio_numero
+      t.string :domicilio_piso
+      t.string :domicilio_depto
+      t.string :domicilio_manzana
+      t.string :domicilio_entre_calle_1
+      t.string :domicilio_entre_calle_2
+      t.string :telefono
+      t.string :otro_telefono
+      t.integer :domicilio_provincia_id
+      t.integer :domicilio_departamento_id
+      t.integer :domicilio_distrito_id
+      t.string :domicilio_localidad
+      t.string :domicilio_barrio_o_paraje
+      t.string :domicilio_codigo_postal
+      t.text :observaciones
+
+      # Lugar de atención habitual
+      t.integer :lugar_de_atencion_habitual_id
+
+      # Datos del adulto responsable del menor (para menores de 14 años)
       t.string :apellido_de_la_madre
       t.string :nombre_de_la_madre
-      t.integer :tipo_de_documento_del_padre_id
-      t.string :numero_de_documento_del_padre
+      t.integer :tipo_de_documento_de_la_madre_id
+      t.string :numero_de_documento_de_la_madre
+      t.integer :alfabetizacion_de_la_madre_id
+      t.integer :alfab_madre_años_ultimo_nivel
       t.string :apellido_del_padre
       t.string :nombre_del_padre
-      t.integer :tipo_de_documento_del_tutor_id
-      t.string :numero_de_documento_del_tutor
+      t.integer :tipo_de_documento_del_padre_id
+      t.string :numero_de_documento_del_padre
+      t.integer :alfabetizacion_del_padre_id
+      t.integer :alfab_padre_años_ultimo_nivel
       t.string :apellido_del_tutor
       t.string :nombre_del_tutor
-      t.date :fecha_de_inscripcion
+      t.integer :tipo_de_documento_del_tutor_id
+      t.string :numero_de_documento_del_tutor
+      t.integer :alfabetizacion_del_tutor_id
+      t.integer :alfab_tutor_años_ultimo_nivel
+
+      # Datos del embarazo y parto (para embarazadas)
+      t.date :fecha_de_la_ultima_menstruacion
       t.date :fecha_de_diagnostico_del_embarazo
       t.integer :semanas_de_embarazo
       t.date :fecha_probable_de_parto
-      t.date :fecha_efectiva_de_parto
+      t.date :fecha_efectiva_de_parto  # Dato mantenido por compatibilidad
+
+      # Score de riesgo cardiovascular del Programa Remediar+Redes
+      t.integer :score_de_riesgo
+
+      # Discapacidad
+      t.string :discapacidad
+
+      # Fecha y centro inscriptor
+      t.date :fecha_de_inscripcion
+      t.references :centro_de_inscripcion
+      t.string :nombre_del_agente_inscriptor
+
+      # Observaciones generales
+      t.text :observaciones_generales
+
+      # Estado de la inscripción al programa
       t.string :activo
-      t.string :domicilio_calle
-      t.string :domicilio_numero
-      t.string :domicilio_manzana
-      t.string :domicilio_piso
-      t.string :domicilio_depto
-      t.string :domicilio_entre_calle_1
-      t.string :domicilio_entre_calle_2
-      t.string :domicilio_barrio_o_paraje
-      t.string :domicilio_municipio
-      t.string :domicilio_departamento_o_partido
-      t.string :domicilio_localidad
-      t.string :domicilio_provincia
-      t.string :domicilio_codigo_postal
-      t.string :telefono
-      t.string :codigo_uad
-      t.string :codigo_ci_uad
       t.integer :motivo_de_la_baja_id
       t.string :mensaje_de_la_baja
-      t.integer :proceso_de_baja_automatica_id
+
+      # Datos relacionados con la carga del registro
       t.datetime :fecha_y_hora_de_carga
       t.string :usuario_que_carga
-      t.string :cuie_del_efector_asignado
-      t.string :cuie_del_lugar_de_atencion_habitual
-      t.string :clave_del_benef_que_provoca_baja
-      t.string :usuario_de_creacion
-      t.date :fecha_de_creacion
-      t.integer :persona_id
-      t.integer :score_de_riesgo
-      t.string :alfabetizacion
-      t.integer :alfabetizacion_anios_ultimo_nivel
-      t.string :alfabetizacion_de_la_madre
-      t.integer :alfab_madre_anios_ultimo_nivel
-      t.string :alfabetizacion_del_padre
-      t.integer :alfab_padre_anios_ultimo_nivel
-      t.string :alfabetizacion_del_tutor
-      t.integer :alfab_tutor_anios_ultimo_nivel
-      t.string :e_mail
-      t.string :numero_de_celular
-      t.date :fecha_de_ultima_menstruacion
-      t.string :observaciones_generales
-      t.string :discapacidad
     end
   end
 end
