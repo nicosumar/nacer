b4b0c5cbcc9d3b45ba4a0abe2417873b1783eb88
Pablo Garcia <pablodgcatulo@gmail.com>
2013-07-18 01:54:05 -0300

    customquery new

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,7 @@ Nacer::Application.routes.draw do
   resources :unidades_de_alta_de_datos, :except => :destroy
   match "informe_de_beneficiarios_activos" => "informes#beneficiarios_activos"
   match "tablero_de_comandos_alto_impacto" => "informes#tablero_de_comandos_alto_impacto"
-  match "usuarios_inscripciones" => "informes#usuarios_inscripciones"
+  match "filtro_reporte" => "informes#filtro_reporte"
 
   root :to => 'inicio#index'
 
