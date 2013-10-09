e8afe6ff803b50922489791a1a06f48fcd8ca080
Pablo Garcia <pablodgcatulo@gmail.com>
2013-10-09 10:41:38 -0300

    lsaa

--- db/migrate/20131008172822_create_liquidaciones_sumar_anexos_administrativos.rb
+++ db/migrate/20131008172822_create_liquidaciones_sumar_anexos_administrativos.rb
@@ -1,9 +1,9 @@
 class CreateLiquidacionesSumarAnexosAdministrativos < ActiveRecord::Migration
   def change
     create_table :liquidaciones_sumar_anexos_administrativos do |t|
-      t.estados_de_los_procesos :references
-      t.fecha_de_inicio :date
-      t.fecha_de_finalizacion :date
+      t.references :estados_de_los_procesos
+      t.date :fecha_de_inicio
+      t.date :fecha_de_finalizacion
 
       t.timestamps
     end
