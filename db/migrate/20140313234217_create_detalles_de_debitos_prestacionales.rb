f76586f93b743aeade0470cfb123ddccb5d9f4ea
Pablo Garcia <pablodgcatulo@gmail.com>
2014-03-14 14:48:19 -0300

    avances

--- db/migrate/20140313234217_create_detalles_de_debitos_prestacionales.rb
+++ db/migrate/20140313234217_create_detalles_de_debitos_prestacionales.rb
@@ -1,28 +1,28 @@
 class CreateDetallesDeDebitosPrestacionales < ActiveRecord::Migration
-	def up
-		create_table :detalles_de_debitos_prestacionales do |t|
-			t.date :fecha_de_debito
-			t.references :prestacion_liquidada
-			t.references :motivo_de_rechazo
-			t.boolean :procesado_para_debito
-			t.boolean :informado_sirge
-			t.text :observaciones
+  def up
+    create_table :detalles_de_debitos_prestacionales do |t|
+      t.date :fecha_de_debito
+      t.references :prestacion_liquidada
+      t.references :motivo_de_rechazo
+      t.boolean :procesado_para_debito
+      t.boolean :informado_sirge
+      t.text :observaciones
 
-			t.timestamps
-		end
+      t.timestamps
+    end         
 
-		execute <<-SQL
-			ALTER TABLE "public"."detalles_de_debitos_prestacionales"
-				ADD FOREIGN KEY ("prestacion_liquidada_id") REFERENCES "public"."prestaciones_liquidadas" ("id") ON DELETE RESTRICT ON UPDATE NO ACTION,
-				ADD FOREIGN KEY ("motivo_de_rechazo_id") REFERENCES "public"."motivos_de_rechazos" ("id") ON DELETE RESTRICT ON UPDATE NO ACTION,
-				ADD UNIQUE ("prestacion_liquidada_id");
+    execute <<-SQL
+      ALTER TABLE "public"."detalles_de_debitos_prestacionales"
+        ADD FOREIGN KEY ("prestacion_liquidada_id") REFERENCES "public"."prestaciones_liquidadas" ("id") ON DELETE RESTRICT ON UPDATE NO ACTION,
+        ADD FOREIGN KEY ("motivo_de_rechazo_id") REFERENCES "public"."motivos_de_rechazos" ("id") ON DELETE RESTRICT ON UPDATE NO ACTION,
+        ADD UNIQUE ("prestacion_liquidada_id");
 
-			CREATE UNIQUE INDEX  ON "public"."detalles_de_debitos_prestacionales" ("prestacion_liquidada_id");
-			CREATE INDEX  ON "public"."detalles_de_debitos_prestacionales" ("motivo_de_rechazo_id");
-		SQL
-	end
+      CREATE UNIQUE INDEX  ON "public"."detalles_de_debitos_prestacionales" ("prestacion_liquidada_id");
+      CREATE INDEX  ON "public"."detalles_de_debitos_prestacionales" ("motivo_de_rechazo_id");
+    SQL
+  end
 
-	def down
-		drop_table :detalles_de_debitos_prestacionales
-	end
+  def down
+    drop_table :detalles_de_debitos_prestacionales
+  end
 end
--- db/migrate/20140314002421_create_tipos_de_debitos_prestacionales.rb
+++ db/migrate/20140314002421_create_tipos_de_debitos_prestacionales.rb
@@ -1,11 +1,40 @@
+# -*- encoding : utf-8 -*-
 class CreateTiposDeDebitosPrestacionales < ActiveRecord::Migration
   def up
     create_table :tipos_de_debitos_prestacionales do |t|
       t.string :nombre
     end
+
+    add_column :detalles_de_debitos_prestacionales, :tipo_de_debito_prestacional_id, "int4"
+
+    execute <<-SQL
+      ALTER TABLE "public"."detalles_de_debitos_prestacionales"
+        ADD FOREIGN KEY ("tipo_de_debito_prestacional_id") 
+          REFERENCES "public"."tipos_de_debitos_prestacionales" ("id") 
+          ON DELETE RESTRICT ON UPDATE NO ACTION;
+
+        CREATE INDEX  ON "public"."detalles_de_debitos_prestacionales" ("tipo_de_debito_prestacional_id"  );
+    SQL
+
+    TipoDeDebitoPrestacional.create([
+      { #ID: 1
+        nombre: "Auditoría Concurrente Externa"
+      },
+      { #ID: 2
+        nombre: "Auditoría Interna - Posterior al pago"
+      }]
+    )
   end
 
   def down
-  	drop_table :tipos_de_debitos_prestacionales
+    execute <<-SQL
+      DROP INDEX "public"."detalles_de_debitos_prestacio_tipo_de_debito_prestacional_i_idx";
+      ALTER TABLE "public"."detalles_de_debitos_prestacionales"
+        DROP CONSTRAINT "detalles_de_debitos_prestacio_tipo_de_debito_prestacional__fkey",
+        DROP COLUMN "tipo_de_debito_prestacional_id";
+    SQL
+    
+    drop_table :tipos_de_debitos_prestacionales
+
   end
 end
