fecd396f594cb0ee96e40fb852f7b390726e51ad
Pablo Garcia <pablodgcatulo@gmail.com>
2014-04-04 12:55:48 -0300

    avances mini

--- app/assets/javascripts/detalles_de_debitos_prestacionales.js
+++ app/assets/javascripts/detalles_de_debitos_prestacionales.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
-  $("# ").change(function(){
+  //inicializo los combos desactivados
+  $("#detalle_de_debito_prestacional_motivo_de_rechazo").select2('enable', false)
+
+  $("#detalle_de_debito_prestacional_prestacion_liquidada_id").change(function(){
     prestacion = $("#detalle_de_debito_prestacional_prestacion_liquidada_id");
 
     if(prestacion.select2('val') == "")
+      $("#detalle_de_debito_prestacional_motivo_de_rechazo").select2('enable', false)
+    else
+      $("#detalle_de_debito_prestacional_motivo_de_rechazo").select2('enable', true)
+
+    
       
   });
 
