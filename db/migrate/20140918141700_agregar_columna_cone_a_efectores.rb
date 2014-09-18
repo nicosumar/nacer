b2a79e98136eca9864c24405401343726f9a31ee
Santiago Bosio <santiago.bosio@gmail.com>
2014-09-18 16:13:43 -0300

    Arreglando un pequeño problema con la migración debido a la versión de PostgreSQL de producción

--- db/migrate/20140918141700_agregar_columna_cone_a_efectores.rb
+++ db/migrate/20140918141700_agregar_columna_cone_a_efectores.rb
@@ -2,7 +2,7 @@ class AgregarColumnaConeAEfectores < ActiveRecord::Migration
   def change
     add_column :efectores, :categorizado_cone, :boolean, :default => false
     execute "
-        ALTER TABLE IF EXISTS ONLY auditorias.auditoria_efectores
+        ALTER TABLE ONLY auditorias.auditoria_efectores
           ADD COLUMN categorizado_cone boolean DEFAULT 'f'::boolean;
       "
     load 'vendor/data/efectores_cone.rb'
