f755a41e65ca8923cefd38036f4f241e9db96948
Santiago Bosio <santiago.bosio@gmail.com>
2015-02-24 08:47:13 -0300

    Arreglo de la migración para solucionar el problema con las tablas de búsquedas locales.

--- db/migrate/20150223200700_modificar_trigger_prestacion_brindada_fts.rb
+++ db/migrate/20150223200700_modificar_trigger_prestacion_brindada_fts.rb
@@ -3,21 +3,54 @@ class ModificarTriggerPrestacionBrindadaFts < ActiveRecord::Migration
     load 'db/sp/trigger_prestaciones_brindadas_fts.rb'
 
     UnidadDeAltaDeDatos.where(facturacion: true).each do |uad|
-      ActiveRecord::Base.connection.execute ""+
-        "DELETE FROM uad_#{uad.codigo}.busquedas_locales WHERE modelo_type = 'PrestacionBrindada' AND modelo_id IN (\n"+
-        "          SELECT pb.id FROM uad_#{uad.codigo}.prestaciones_brindadas pb JOIN estados_de_las_prestaciones ep ON (ep.id = pb.estado_de_la_prestacion_id)\n"+
-        "            WHERE NOT ep.indexable);\n"+
-        "INSERT INTO uad_#{uad.codigo}.busquedas_locales (modelo_type, modelo_id)\n"+
-        "  SELECT 'PrestacionBrindada', pb.id FROM\n"+
-        "  uad_#{uad.codigo}.prestaciones_brindadas pb JOIN estados_de_las_prestaciones ep ON (ep.id = pb.estado_de_la_prestacion_id)\n"+
-        "  WHERE ep.indexable AND NOT EXISTS (\n"+
-        "    SELECT * FROM uad_#{uad.codigo}.busquedas_locales bl WHERE bl.modelo_type = 'PrestacionBrindada' AND bl.modelo_id = pb.id\n"+
-        "            );\n"+
-        "UPDATE uad_#{uad.codigo}.prestaciones_brindadas SET estado_de_la_prestacion_id = estado_de_la_prestacion_id;"
+      ActiveRecord::Base.connection.execute "
+        UPDATE uad_#{uad.codigo}.prestaciones_brindadas
+          SET estado_de_la_prestacion_id = 11, observaciones_de_liquidacion = 'Prestación duplicada'
+          WHERE id IN (
+            SELECT id
+              FROM uad_#{uad.codigo}.prestaciones_brindadas pb1
+              WHERE EXISTS (
+                SELECT *
+                  FROM uad_#{uad.codigo}.prestaciones_brindadas pb2
+                  WHERE
+                    pb1.clave_de_beneficiario = pb2.clave_de_beneficiario
+                    AND pb1.prestacion_id = pb2.prestacion_id
+                    AND pb1.fecha_de_la_prestacion = pb2.fecha_de_la_prestacion
+                    AND pb1.estado_de_la_prestacion_id = pb2.estado_de_la_prestacion_id
+                    AND pb1 > pb2
+              )
+          );
+      "
+      ActiveRecord::Base.connection.execute "
+        DELETE FROM uad_#{uad.codigo}.busquedas_locales
+          WHERE
+            modelo_type = 'PrestacionBrindada'
+            AND modelo_id IN (
+              SELECT pb.id
+                FROM
+                  uad_#{uad.codigo}.prestaciones_brindadas pb
+                  JOIN estados_de_las_prestaciones ep ON (ep.id = pb.estado_de_la_prestacion_id)
+                WHERE NOT ep.indexable
+            );
+      "
+      ActiveRecord::Base.connection.execute "
+        INSERT INTO uad_#{uad.codigo}.busquedas_locales (modelo_type, modelo_id)
+          SELECT 'PrestacionBrindada', pb.id
+            FROM
+              uad_#{uad.codigo}.prestaciones_brindadas pb
+              JOIN estados_de_las_prestaciones ep ON (ep.id = pb.estado_de_la_prestacion_id)
+            WHERE
+              ep.indexable
+              AND NOT EXISTS (
+                SELECT *
+                  FROM uad_#{uad.codigo}.busquedas_locales bl
+                  WHERE bl.modelo_type = 'PrestacionBrindada' AND bl.modelo_id = pb.id
+              );
+      "
+      ActiveRecord::Base.connection.execute "
+        UPDATE uad_#{uad.codigo}.prestaciones_brindadas SET estado_de_la_prestacion_id = estado_de_la_prestacion_id;
+      "
     end
-      
-
-
   end
 
   def down
--- db/sp/trigger_prestaciones_brindadas_fts.rb
+++ db/sp/trigger_prestaciones_brindadas_fts.rb
@@ -38,26 +38,30 @@ ActiveRecord::Base.connection.execute <<-SQL
           IF (TG_OP = 'DELETE') THEN
             -- Eliminar el registro asociado en la tabla de búsquedas
             EXECUTE '
-              DELETE FROM ' || TG_TABLE_SCHEMA || '.busquedas_locales WHERE modelo_type = ''PrestacionBrindada'' AND modelo_id = ' || OLD.id || ';
-            ';
+              DELETE
+                FROM ' || quote_ident(TG_TABLE_SCHEMA) || '.busquedas_locales
+                WHERE modelo_type = ' || quote_literal('PrestacionBrindada') || ' AND modelo_id = ' || OLD.id || ';';
             RETURN OLD;
           ELSIF (TG_OP = 'UPDATE' AND NOT new_indexable AND old_indexable) THEN
             -- Eliminar el registro si es una actualización y la novedad ya no debe indexarse en las búsquedas
             EXECUTE '
-              DELETE FROM ' || TG_TABLE_SCHEMA || '.busquedas_locales WHERE modelo_type = ''PrestacionBrindada'' AND modelo_id = ' || NEW.id || ';
-            ';
+              DELETE
+                FROM ' || quote_ident(TG_TABLE_SCHEMA) || '.busquedas_locales
+                WHERE modelo_type = ' || quote_literal('PrestacionBrindada') || ' AND modelo_id = ' || NEW.id || ';';
             RETURN NEW;
           ELSIF (TG_OP = 'UPDATE' AND new_indexable AND old_indexable) THEN
             -- Extraemos los datos del afiliado de la tabla de novedades (si encontramos una pendiente), o de la de afiliados
-            SELECT * INTO afiliado FROM afiliados WHERE clave_de_beneficiario = NEW.clave_de_beneficiario;
+            SELECT * INTO afiliado
+              FROM afiliados
+              WHERE clave_de_beneficiario = NEW.clave_de_beneficiario;
             EXECUTE '
               SELECT *
-                FROM ' || TG_TABLE_SCHEMA || '.novedades_de_los_afiliados novedades
+                FROM ' || quote_ident(TG_TABLE_SCHEMA) || '.novedades_de_los_afiliados novedades
                 LEFT JOIN estados_de_las_novedades estados
                   ON (estados.id = novedades.estado_de_la_novedad_id)
                 WHERE
-                  clave_de_beneficiario = ''' || COALESCE(NEW.clave_de_beneficiario, '') || ''' AND estados.pendiente
-            ' INTO novedad;
+                  clave_de_beneficiario = ' || quote_literal(COALESCE(NEW.clave_de_beneficiario, '')) ||
+                  ' AND estados.pendiente' INTO novedad;
             IF novedad.clave_de_beneficiario IS NOT NULL THEN
               apellido := novedad.apellido;
               nombre := novedad.nombre;
@@ -80,66 +84,67 @@ ActiveRecord::Base.connection.execute <<-SQL
 
             -- Actualizar el registro asociado en la tabla de búsquedas locales si el registro estaba indexado
             EXECUTE '
-              UPDATE ' || TG_TABLE_SCHEMA || '.busquedas_locales SET
-                titulo =
-                  ''Prestación pendiente: ' ||
-                  COALESCE(apellido || ', ', '') ||
-                  COALESCE(nombre, '') ||
-                  ' (' || codigo_de_prestacion || COALESCE(codigo_de_diagnostico, '') || ' - ' ||
-                  COALESCE(to_char(NEW.fecha_de_la_prestacion, 'DD/MM/YYYY'), '') || ')'',
-                texto =
-                  ''Código: ' ||
-                  codigo_de_prestacion || COALESCE(codigo_de_diagnostico, '') ||
-                  ', prestación: ' ||
-                  prestacion ||
-                  ', diagnóstico: ' ||
-                  COALESCE(diagnostico, '') ||
-                  ', fecha de la prestación: ' ||
-                  COALESCE(to_char(NEW.fecha_de_la_prestacion, 'DD/MM/YYYY'), '') ||
-                  ', beneficiario: ' ||
-                  COALESCE(apellido || ', ', '') || COALESCE(nombre, '') ||
-                  ', clave ''''' ||
-                  COALESCE(NEW.clave_de_beneficiario, '') ||
-                  ''''', documento ' ||
-                  COALESCE(clase_de_documento || ' ', '') ||
-                  COALESCE(tipo_de_documento || ' ', '') ||
-                  COALESCE(numero_de_documento, '') ||
-                  ', fecha de nacimiento: ' ||
-                  COALESCE(to_char(fecha_de_nacimiento, 'DD/MM/YYYY'), '') ||
-                  '.'',
-                vector_fts = ''' ||
-                  replace((setweight(to_tsvector('public.indices_fts', 'Código: '), 'D') ||
-                  setweight(to_tsvector('public.indices_fts', codigo_de_prestacion || COALESCE(codigo_de_diagnostico, '')), 'A') ||
-                  setweight(to_tsvector('public.indices_fts', ', prestación: '), 'D') ||
-                  setweight(to_tsvector('public.indices_fts', prestacion), 'A') ||
-                  setweight(to_tsvector('public.indices_fts', ', diagnóstico: '), 'D') ||
-                  setweight(to_tsvector('public.indices_fts', COALESCE(diagnostico, '')), 'A') ||
-                  setweight(to_tsvector('public.indices_fts', ', fecha de la prestación: '), 'D') ||
-                  setweight(to_tsvector('public.indices_fts', COALESCE(to_char(NEW.fecha_de_la_prestacion, 'DD/MM/YYYY'), '')), 'A') ||
-                  setweight(to_tsvector('public.indices_fts', ', beneficiario: '), 'D') ||
-                  setweight(to_tsvector('public.indices_fts', COALESCE(apellido || ', ', '') || COALESCE(nombre, '')), 'B') ||
-                  setweight(to_tsvector('public.indices_fts', ', clave '''''), 'D') ||
-                  setweight(to_tsvector('public.indices_fts', COALESCE(NEW.clave_de_beneficiario, '')), 'B') ||
-                  setweight(to_tsvector('public.indices_fts', ''''', documento '), 'D') ||
-                  setweight(to_tsvector('public.indices_fts', COALESCE(clase_de_documento || ' ', '')), 'C') ||
-                  setweight(to_tsvector('public.indices_fts', COALESCE(tipo_de_documento || ' ', '')), 'C') ||
-                  setweight(to_tsvector('public.indices_fts', COALESCE(numero_de_documento, '')), 'B') ||
-                  setweight(to_tsvector('public.indices_fts', ', fecha de nacimiento: '), 'D') ||
-                  setweight(to_tsvector('public.indices_fts', COALESCE(to_char(fecha_de_nacimiento, 'DD/MM/YYYY'), '')), 'C'))::text, '''', '''''') || '''::tsvector
-                WHERE modelo_type = ''PrestacionBrindada'' AND modelo_id = ' || NEW.id || ';
-            ';
+              UPDATE ' || quote_ident(TG_TABLE_SCHEMA) || '.busquedas_locales SET
+                titulo = ' ||
+                  quote_nullable('Prestación pendiente: ' ||
+                    COALESCE(apellido || ', ', '') ||
+                    COALESCE(nombre, '') ||
+                    ' (' || codigo_de_prestacion || COALESCE(codigo_de_diagnostico, '') || ' - ' ||
+                    COALESCE(to_char(NEW.fecha_de_la_prestacion, 'DD/MM/YYYY'), '') || ')') || ',
+                texto = ' ||
+                  quote_nullable('Código: ' ||
+                    codigo_de_prestacion || COALESCE(codigo_de_diagnostico, '') ||
+                    ', prestación: ' ||
+                    prestacion ||
+                    ', diagnóstico: ' ||
+                    COALESCE(diagnostico, '') ||
+                    ', fecha de la prestación: ' ||
+                    COALESCE(to_char(NEW.fecha_de_la_prestacion, 'DD/MM/YYYY'), '') ||
+                    ', beneficiario: ' ||
+                    COALESCE(apellido || ', ', '') || COALESCE(nombre, '') ||
+                    ', clave ''' ||
+                    COALESCE(NEW.clave_de_beneficiario, '') ||
+                    ''', documento ' ||
+                    COALESCE(clase_de_documento || ' ', '') ||
+                    COALESCE(tipo_de_documento || ' ', '') ||
+                    COALESCE(numero_de_documento, '') ||
+                    ', fecha de nacimiento: ' ||
+                    COALESCE(to_char(fecha_de_nacimiento, 'DD/MM/YYYY'), '') ||
+                    '.') || ',
+                vector_fts = ' ||
+                  quote_nullable((setweight(to_tsvector('public.indices_fts', 'Código: '), 'D') ||
+                    setweight(to_tsvector('public.indices_fts', codigo_de_prestacion || COALESCE(codigo_de_diagnostico, '')), 'A') ||
+                    setweight(to_tsvector('public.indices_fts', ', prestación: '), 'D') ||
+                    setweight(to_tsvector('public.indices_fts', prestacion), 'A') ||
+                    setweight(to_tsvector('public.indices_fts', ', diagnóstico: '), 'D') ||
+                    setweight(to_tsvector('public.indices_fts', COALESCE(diagnostico, '')), 'A') ||
+                    setweight(to_tsvector('public.indices_fts', ', fecha de la prestación: '), 'D') ||
+                    setweight(to_tsvector('public.indices_fts', COALESCE(to_char(NEW.fecha_de_la_prestacion, 'DD/MM/YYYY'), '')), 'A') ||
+                    setweight(to_tsvector('public.indices_fts', ', beneficiario: '), 'D') ||
+                    setweight(to_tsvector('public.indices_fts', COALESCE(apellido || ', ', '') || COALESCE(nombre, '')), 'B') ||
+                    setweight(to_tsvector('public.indices_fts', ', clave '''''), 'D') ||
+                    setweight(to_tsvector('public.indices_fts', COALESCE(NEW.clave_de_beneficiario, '')), 'B') ||
+                    setweight(to_tsvector('public.indices_fts', ''''', documento '), 'D') ||
+                    setweight(to_tsvector('public.indices_fts', COALESCE(clase_de_documento || ' ', '')), 'C') ||
+                    setweight(to_tsvector('public.indices_fts', COALESCE(tipo_de_documento || ' ', '')), 'C') ||
+                    setweight(to_tsvector('public.indices_fts', COALESCE(numero_de_documento, '')), 'B') ||
+                    setweight(to_tsvector('public.indices_fts', ', fecha de nacimiento: '), 'D') ||
+                    setweight(to_tsvector('public.indices_fts', COALESCE(to_char(fecha_de_nacimiento, 'DD/MM/YYYY'), '')), 'C'))::text) || '::tsvector
+                WHERE modelo_type = ' || quote_literal('PrestacionBrindada') || ' AND modelo_id = ' || NEW.id || ';';
             RETURN NEW;
           ELSIF (new_indexable AND (TG_OP = 'INSERT' OR TG_OP = 'UPDATE' AND NOT old_indexable)) THEN
             -- Extraemos los datos del afiliado de la tabla de novedades (si encontramos una pendiente), o de la de afiliados
-            SELECT * INTO afiliado FROM afiliados WHERE clave_de_beneficiario = NEW.clave_de_beneficiario;
+            SELECT * INTO afiliado
+              FROM afiliados
+              WHERE clave_de_beneficiario = NEW.clave_de_beneficiario;
             EXECUTE '
               SELECT *
-                FROM ' || TG_TABLE_SCHEMA || '.novedades_de_los_afiliados novedades
+                FROM ' || quote_ident(TG_TABLE_SCHEMA) || '.novedades_de_los_afiliados novedades
                 LEFT JOIN estados_de_las_novedades estados
                   ON (estados.id = novedades.estado_de_la_novedad_id)
                 WHERE
-                  clave_de_beneficiario = ''' || COALESCE(NEW.clave_de_beneficiario, '') || ''' AND estados.pendiente
-            ' INTO novedad;
+                  clave_de_beneficiario = ' || quote_literal(COALESCE(NEW.clave_de_beneficiario, '')) ||
+                  ' AND estados.pendiente' INTO novedad;
             IF novedad.clave_de_beneficiario IS NOT NULL THEN
               apellido := novedad.apellido;
               nombre := novedad.nombre;
@@ -162,52 +167,51 @@ ActiveRecord::Base.connection.execute <<-SQL
 
             -- Insertar el registro asociado en la tabla de búsquedas locales
             EXECUTE '
-              INSERT INTO ' || TG_TABLE_SCHEMA || '.busquedas_locales (modelo_type, modelo_id, titulo, texto, vector_fts) VALUES (
-                ''PrestacionBrindada'', ' ||
-                NEW.id || ',
-                ''Prestación pendiente: ' ||
-                COALESCE(apellido || ', ', '') ||
-                COALESCE(nombre, '') ||
-                ' (' || codigo_de_prestacion || COALESCE(codigo_de_diagnostico, '') || ' - ' ||
-                COALESCE(to_char(NEW.fecha_de_la_prestacion, 'DD/MM/YYYY'), '') || ')'',
-                ''Código: ' ||
-                codigo_de_prestacion || COALESCE(codigo_de_diagnostico, '') ||
-                ', prestación: ' ||
-                prestacion ||
-                ', diagnóstico: ' ||
-                COALESCE(diagnostico, '') ||
-                ', fecha de la prestación: ' ||
-                COALESCE(to_char(NEW.fecha_de_la_prestacion, 'DD/MM/YYYY'), '') ||
-                ', beneficiario: ' ||
-                COALESCE(apellido || ', ', '') || COALESCE(nombre, '') ||
-                ', clave ''''' ||
-                COALESCE(NEW.clave_de_beneficiario, '') ||
-                ''''', documento ' ||
-                COALESCE(clase_de_documento || ' ', '') ||
-                COALESCE(tipo_de_documento || ' ', '') ||
-                COALESCE(numero_de_documento, '') ||
-                ', fecha de nacimiento: ' ||
-                COALESCE(to_char(fecha_de_nacimiento, 'DD/MM/YYYY'), '') ||
-                '.'', ''' ||
-                replace((setweight(to_tsvector('public.indices_fts', 'Código: '), 'D') ||
-                setweight(to_tsvector('public.indices_fts', codigo_de_prestacion || COALESCE(codigo_de_diagnostico, '')), 'A') ||
-                setweight(to_tsvector('public.indices_fts', ', prestación: '), 'D') ||
-                setweight(to_tsvector('public.indices_fts', prestacion), 'A') ||
-                setweight(to_tsvector('public.indices_fts', ', diagnóstico: '), 'D') ||
-                setweight(to_tsvector('public.indices_fts', COALESCE(diagnostico, '')), 'A') ||
-                setweight(to_tsvector('public.indices_fts', ', fecha de la prestación: '), 'D') ||
-                setweight(to_tsvector('public.indices_fts', COALESCE(to_char(NEW.fecha_de_la_prestacion, 'DD/MM/YYYY'), '')), 'A') ||
-                setweight(to_tsvector('public.indices_fts', ', beneficiario: '), 'D') ||
-                setweight(to_tsvector('public.indices_fts', COALESCE(apellido || ', ', '') || COALESCE(nombre, '')), 'B') ||
-                setweight(to_tsvector('public.indices_fts', ', clave '''''), 'D') ||
-                setweight(to_tsvector('public.indices_fts', COALESCE(NEW.clave_de_beneficiario, '')), 'B') ||
-                setweight(to_tsvector('public.indices_fts', ''''', documento '), 'D') ||
-                setweight(to_tsvector('public.indices_fts', COALESCE(clase_de_documento || ' ', '')), 'C') ||
-                setweight(to_tsvector('public.indices_fts', COALESCE(tipo_de_documento || ' ', '')), 'C') ||
-                setweight(to_tsvector('public.indices_fts', COALESCE(numero_de_documento, '')), 'B') ||
-                setweight(to_tsvector('public.indices_fts', ', fecha de nacimiento: '), 'D') ||
-                setweight(to_tsvector('public.indices_fts', COALESCE(to_char(fecha_de_nacimiento, 'DD/MM/YYYY'), '')), 'C'))::text, '''', '''''') || '''::tsvector);
-            ';
+              INSERT INTO ' || quote_ident(TG_TABLE_SCHEMA) || '.busquedas_locales (modelo_type, modelo_id, titulo, texto, vector_fts) VALUES ( ' ||
+                quote_literal('PrestacionBrindada') || ', ' ||
+                NEW.id || ', ' ||
+                quote_nullable('Prestación pendiente: ' ||
+                  COALESCE(apellido || ', ', '') ||
+                  COALESCE(nombre, '') ||
+                  ' (' || codigo_de_prestacion || COALESCE(codigo_de_diagnostico, '') || ' - ' ||
+                  COALESCE(to_char(NEW.fecha_de_la_prestacion, 'DD/MM/YYYY'), '') || ')') || ', ' ||
+                quote_nullable('Código: ' ||
+                  codigo_de_prestacion || COALESCE(codigo_de_diagnostico, '') ||
+                  ', prestación: ' ||
+                  prestacion ||
+                  ', diagnóstico: ' ||
+                  COALESCE(diagnostico, '') ||
+                  ', fecha de la prestación: ' ||
+                  COALESCE(to_char(NEW.fecha_de_la_prestacion, 'DD/MM/YYYY'), '') ||
+                  ', beneficiario: ' ||
+                  COALESCE(apellido || ', ', '') || COALESCE(nombre, '') ||
+                  ', clave ''' ||
+                  COALESCE(NEW.clave_de_beneficiario, '') ||
+                  ''', documento ' ||
+                  COALESCE(clase_de_documento || ' ', '') ||
+                  COALESCE(tipo_de_documento || ' ', '') ||
+                  COALESCE(numero_de_documento, '') ||
+                  ', fecha de nacimiento: ' ||
+                  COALESCE(to_char(fecha_de_nacimiento, 'DD/MM/YYYY'), '') ||
+                  '.') || ', ' ||
+                quote_nullable((setweight(to_tsvector('public.indices_fts', 'Código: '), 'D') ||
+                  setweight(to_tsvector('public.indices_fts', codigo_de_prestacion || COALESCE(codigo_de_diagnostico, '')), 'A') ||
+                  setweight(to_tsvector('public.indices_fts', ', prestación: '), 'D') ||
+                  setweight(to_tsvector('public.indices_fts', prestacion), 'A') ||
+                  setweight(to_tsvector('public.indices_fts', ', diagnóstico: '), 'D') ||
+                  setweight(to_tsvector('public.indices_fts', COALESCE(diagnostico, '')), 'A') ||
+                  setweight(to_tsvector('public.indices_fts', ', fecha de la prestación: '), 'D') ||
+                  setweight(to_tsvector('public.indices_fts', COALESCE(to_char(NEW.fecha_de_la_prestacion, 'DD/MM/YYYY'), '')), 'A') ||
+                  setweight(to_tsvector('public.indices_fts', ', beneficiario: '), 'D') ||
+                  setweight(to_tsvector('public.indices_fts', COALESCE(apellido || ', ', '') || COALESCE(nombre, '')), 'B') ||
+                  setweight(to_tsvector('public.indices_fts', ', clave '''''), 'D') ||
+                  setweight(to_tsvector('public.indices_fts', COALESCE(NEW.clave_de_beneficiario, '')), 'B') ||
+                  setweight(to_tsvector('public.indices_fts', ''''', documento '), 'D') ||
+                  setweight(to_tsvector('public.indices_fts', COALESCE(clase_de_documento || ' ', '')), 'C') ||
+                  setweight(to_tsvector('public.indices_fts', COALESCE(tipo_de_documento || ' ', '')), 'C') ||
+                  setweight(to_tsvector('public.indices_fts', COALESCE(numero_de_documento, '')), 'B') ||
+                  setweight(to_tsvector('public.indices_fts', ', fecha de nacimiento: '), 'D') ||
+                  setweight(to_tsvector('public.indices_fts', COALESCE(to_char(fecha_de_nacimiento, 'DD/MM/YYYY'), '')), 'C'))::text) || '::tsvector);';
             RETURN NEW;
           END IF;
           RETURN NULL;
