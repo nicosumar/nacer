6fd49f971a65a8ef8f24ebee7a9699fc921a8604
Matias Martinez <matimartinez91@gmail.com>
2017-05-15 10:48:10 -0300

    Fix en listado de prestaciones y se agrega atributo a delayed_job

--- app/services/prestacion_service.rb
+++ app/services/prestacion_service.rb
@@ -8,7 +8,7 @@ class PrestacionService
       linea_de_cuidado = { }
       
       prestaciones.each do |prestacion|
-        if prestacion.prestaciones_pdss.present?
+        if prestacion.prestaciones_pdss.present? and (prestacion.prestaciones_pdss.first.persisted?)
           prestacion.prestaciones_pdss.each do |prestacion_pdss|
             seccion_pdss_id = prestacion_pdss.grupo_pdss.seccion_pdss.id
             grupo_pdss_id = prestacion_pdss.grupo_pdss.id
@@ -53,40 +53,6 @@ class PrestacionService
               seccion_grupo_pdss[:prestaciones_count] += 1
               seccion_pdss[:prestaciones_count] += 1
             end
-          end
-        else
-          seccion_pdss_array = secciones_pdss.select {|seccion_pdss| seccion_pdss[:seccion_pdss_id] == seccion_pdss_id }
-          seccion_pdss = seccion_pdss_array.first
-
-          if seccion_pdss == nil
-            seccion_pdss = { :seccion_pdss_id => 0, :nombre => "Sin especificar", :secciones_grupo_pdss =>  [], :prestaciones_count => 0 }
-            seccion_pdss[:seccion_pdss_id] = prestacion_pdss.grupo_pdss.seccion_pdss.id
-            seccion_pdss[:nombre] = prestacion_pdss.grupo_pdss.seccion_pdss.nombre
-            seccion_pdss[:secciones_grupo_pdss] = []
-            secciones_pdss << seccion_pdss
-          end
-
-          seccion_grupo_pdss_array = seccion_pdss[:secciones_grupo_pdss].select {|seccion_grupo_pdss| seccion_grupo_pdss[:seccion_pdss_id] == 0 && seccion_grupo_pdss[:grupo_pdss_id] == 0 }
-          seccion_grupo_pdss = seccion_grupo_pdss_array.first
-          if seccion_grupo_pdss == nil
-            seccion_grupo_pdss = { :seccion_pdss_id => 0, :grupo_pdss_id => 0, :nombre => "Sin especificar", :lineas_de_cuidado =>  [], :prestaciones_count => 0 }
-            seccion_grupo_pdss[:lineas_de_cuidado] =  []
-            secciones_grupo_pdss << seccion_grupo_pdss
-          end
-
-          lineas_de_cuidado_array = seccion_grupo_pdss[:lineas_de_cuidado].select {|linea_de_cuidado| linea_de_cuidado[:id] == 0 }
-          linea_de_cuidado = lineas_de_cuidado_array.first
-          if linea_de_cuidado == nil
-            linea_de_cuidado = { :id => 0, :nombre => "Sin especificar", :prestaciones =>  [] }
-            linea_de_cuidado[:prestaciones] = []
-            seccion_grupo_pdss[:lineas_de_cuidado] << linea_de_cuidado
-          end
-          
-          prestaciones_array = linea_de_cuidado[:prestaciones].select {|p| p[:id] == prestacion.id }
-          if prestaciones_array.blank?
-            linea_de_cuidado[:prestaciones] << prestacion 
-            seccion_grupo_pdss[:prestaciones_count] += 1
-            seccion_pdss[:prestaciones_count] += 1
           end
         end
       end   
