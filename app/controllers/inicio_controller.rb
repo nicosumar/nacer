f8e061c2d3656bff17fa118776a0659fbdc3c015
Nico <nicorecab@gmail.com>
2017-07-18 12:42:04 -0300

    agregué un if para que se pueda ver la parte de informes de rendicion si se ingresa con alguno de los dos efectores con los que vamos a probar el sistema. También cambié unas cositas en las notificaciones de las advertencias (botones y arreglé lo del orden)

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -16,7 +16,7 @@ class InicioController < ApplicationController
 
   		else
   			
-        @notificaciones = Notificacion.where("unidad_de_alta_de_datos_id = " + @uad_actual.id.to_s + " AND fecha_lectura IS NULL").paginate(:page => params[:page], :per_page => 20, :order => "fecha_evento DESC")
+        @notificaciones = Notificacion.where("unidad_de_alta_de_datos_id = " + @uad_actual.id.to_s + " AND fecha_lectura IS NULL").paginate(:page => params[:page], :per_page => 20, :order => "fecha_evento DESC, created_at")
 
   		end
   	end
@@ -75,7 +75,7 @@ class InicioController < ApplicationController
   	@uad_actual.fecha_ultimas_notificaciones = Time.now
   	@uad_actual.save!
 
-  	@notificaciones = Notificacion.where(:unidad_de_alta_de_datos_id => @uad_actual.id).paginate(:page => params[:page], :per_page => 20, :order => "fecha_evento DESC")
+  	@notificaciones = Notificacion.where(:unidad_de_alta_de_datos_id => @uad_actual.id).paginate(:page => params[:page], :per_page => 20, :order => "fecha_evento DESC, created_at")
 
   end
 
